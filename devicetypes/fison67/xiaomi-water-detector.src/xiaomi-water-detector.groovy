20e1a4caf48a8f187b9d8b02759340069decf10c
chals <fison67@nate.com>
2018-10-21 11:54:20 +0900

    Create xiaomi-water-detector.groovy

---
+++ devicetypes/fison67/xiaomi-water-detector.src/xiaomi-water-detector.groovy
@@ -0,0 +1,144 @@
+/**
+ *  Xiaomi Water Dectector (v.0.0.1)
+ *
+ * MIT License
+ *
+ * Copyright (c) 2018 fison67@nate.com
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+ 
+import groovy.json.JsonSlurper
+
+metadata {
+	definition (name: "Xiaomi Water Detector", namespace: "fison67", author: "fison67") {
+        capability "Sensor"
+        capability "Water Sensor"     //  ["dry", "wet"]
+        capability "Refresh"
+        
+        attribute "battery", "string"
+        attribute "lastCheckin", "Date"
+        
+	}
+
+	simulator {
+	}
+
+	tiles {
+		multiAttributeTile(name:"water", type: "generic", width: 6, height: 4){
+			tileAttribute ("device.water", key: "PRIMARY_CONTROL") {
+               	attributeState "dry", label:'${name}', icon:"https://raw.githubusercontent.com/fison67/mi_connector/master/icons/water_dry.png" , backgroundColor:"#ffffff"
+            	attributeState "wet", label:'${name}', icon:"https://raw.githubusercontent.com/fison67/mi_connector/master/icons/water_wet.png" , backgroundColor:"#ffffff"
+			}
+            tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
+    			attributeState("default", label:'Last Update: ${currentValue}',icon: "st.Health & Wellness.health9")
+            }
+		}
+        
+        valueTile("battery", "device.battery", width: 2, height: 2) {
+            state "val", label:'${currentValue}%', defaultState: true
+        }
+                
+        standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
+            state "default", label:"", action:"refresh", icon:"st.secondary.refresh"
+        }
+	}
+}
+
+// parse events into attributes
+def parse(String description) {
+	log.debug "Parsing '${description}'"
+}
+
+def setInfo(String app_url, String id) {
+	log.debug "${app_url}, ${id}"
+	state.app_url = app_url
+    state.id = id
+}
+
+def setStatus(params){
+	log.debug "${params.key} : ${params.data}"
+ 	switch(params.key){
+    case "waterDetected":
+    	sendEvent(name:"water", value: (params.data == "true" ? "wet" : "dry") )
+    	break;
+    case "batteryLevel":
+    	sendEvent(name:"battery", value: params.data)
+    	break;
+    }
+    
+    updateLastTime()
+}
+
+def updateLastTime(){
+	def now = new Date().format("yyyy-MM-dd HH:mm:ss", location.timeZone)
+    sendEvent(name: "lastCheckin", value: now)
+}
+
+def callback(hubitat.device.HubResponse hubResponse){
+	def msg
+    try {
+        msg = parseLanMessage(hubResponse.description)
+		def jsonObj = new JsonSlurper().parseText(msg.body)
+        
+        sendEvent(name:"battery", value: jsonObj.properties.batteryLevel)
+        sendEvent(name:"water", value: jsonObj.properties.waterDetected ? "wet" : "dry")
+        
+        updateLastTime()
+    } catch (e) {
+        log.error "Exception caught while parsing data: "+e;
+    }
+}
+
+def updated() {
+}
+
+def refresh(){
+	log.debug "Refresh"
+    def options = [
+     	"method": "GET",
+        "path": "/devices/get/${state.id}",
+        "headers": [
+        	"HOST": state.app_url,
+            "Content-Type": "application/json"
+        ]
+    ]
+    sendCommand(options, callback)
+}
+
+def sendCommand(options, _callback){
+	def myhubAction = new hubitat.device.HubAction(options, null, [callback: _callback])
+    sendHubCommand(myhubAction)
+}
+
+def makeCommand(body){
+	def options = [
+     	"method": "POST",
+        "path": "/control",
+        "headers": [
+        	"HOST": state.app_url,
+            "Content-Type": "application/json"
+        ],
+        "body":body
+    ]
+    return options
+}
